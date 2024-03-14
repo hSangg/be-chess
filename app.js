cc059af73d614ac036dfff13865194a204018a18
thithanhcong102 <thithanhcong102@gmail.com>
2024-03-14 21:10:25 +0700

    connected to mongoose

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 import express from 'express'
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser'
 import usersRoutes from './routes/users.js'
-const app = express();
+
 const PORT = 5000;
 
-app.use(bodyParser.json());
 
-app.use('/users', usersRoutes);
-app.get('/', (req, res) => {
-    console.log("test");
-    res.send('hello from homepage');
-});
+const app = express();
+mongoose.connect('mongodb://localhost:27017/MobileDb')
+
+const AccountSchema = mongoose.Schema({
+    account: String,
+    password: String,
+})
+
+const AccountModel = mongoose.model("orders", AccountSchema);
+app.get("/getAccounts", (req, res) => {
+    AccountModel.find({}).then(function (accs) {
+        res.json(accs)
+    }).catch(function (err) {
+        console.log(err)
+    })
+})
+app.listen(PORT, () => {
+    console.log("server is running")
+})
 
 
-app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`))
