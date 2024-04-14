40a5699139c7eb9d64647d49449bf60ee8dac7b0
sang <sangfc774@gmail.com>
2024-04-14 10:18:01 +0700

    add cors + fix missing case of login

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-
-import express from 'express'
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser'
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 import fetch from "node-fetch";
-import dotenv from 'dotenv/config'
-import router from './routes/index.route.js'
-
-
-
+import dotenv from "dotenv/config";
+import cors from "cors";
+import router from "./routes/index.route.js";
 
 const app = express();
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(express.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 app.use(bodyParser.json());
+app.use(cors());
+
 await mongoose
-    .connect(process.env.MONGO_URI).then(() => {
-        console.info("Connected to the Db")
-    })
-    .catch((e) => {
-        console.log("Error", e)
-    });
-
-
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.info("Connected to the Db");
+  })
+  .catch((e) => {
+    console.log("Error", e);
+  });
 
 //router
-app.use('', router)
+app.use("", router);
 app.listen(process.env.PORT, () => {
-    console.log("listening on port ", process.env.PORT);
-})
+  console.log("listening on port ", process.env.PORT);
+});
--- routes/Auth.route.js
+++ routes/Auth.route.js
@@ -1,10 +1,9 @@
-
-import express from 'express'
-import { register, login } from '../controllers/Auth.controller.js';
+import express from "express";
+import { register, login } from "../controllers/Auth.controller.js";
 
 const router = express.Router();
 
-router.post('/signUp', register)
-router.post('/signIn', login)
+router.post("/signUp", register);
+router.post("/signIn", login);
 
-export default router
+export default router;
