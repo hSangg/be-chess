c3276819f57c7cd43c775adecad5436aefff56ac
Tramy21522352 <nttm.04062003@gmail.com>
2024-05-05 15:35:00 +0700

    changed send string  password to 6 digit number

--- utils/function.js
+++ utils/function.js
@@ -9,9 +9,9 @@ const generateRandomFiveDigitNumberAsString = () => {
 
 const generateRandomPassword = () => {
   let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const characters = '0123456789';
   const charactersLength = characters.length;
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
