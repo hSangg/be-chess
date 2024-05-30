93bb1f292e90bcd4fcaffa36ffaf6e6218a51fec
Aransu <thithanhcong102@gmail.com>
2024-05-30 18:41:42 +0700

    fixed some bugs

--- services/Game.service.js
+++ services/Game.service.js
@@ -1,28 +1,29 @@
 import gameModel from "../models/game.model.js";
 
 const saveGameService = async (req, res) => {
-    const pieceDataList = req.body;
-    console.log(pieceDataList)
-    const count = await gameModel.countDocuments({});
     try {
+        const pieceDataList = req.body;
+
+        const count = await gameModel.countDocuments({});
+
         const game = new gameModel({
             game: count ? count : 0,
             pieces: pieceDataList
-        })
-        await game.save()
+        });
+        await game.save();
+
         res.status(201).send({
             status: 201,
             message: "Game saved"
-        })
-    }
-    catch (err) {
+        });
+    } catch (err) {
         res.status(400).send({
             status: 400,
-            message: "An error occur",
+            message: "An error occurred",
             error: err.message
-        })
+        });
     }
-}
+};
 
 const loadGameService = async (req, res) => {
     try {
